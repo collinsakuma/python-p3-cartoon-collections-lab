4f3e44b09ac80d0253f9f86ee010eb81f3d781ff
Collin Sakuma <collinmsakuma@gmail.com>
2023-03-14 17:02:00 -0700

    complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,30 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f"{dwarves.index(dwarf) + 1}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteers):
+    return [planeteer.title() + "!" for planeteer in planeteers]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+        if (len(word) > 4):
+            return True
+    else:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    # for snack in snacks:
+    #     if (snack == "cheddar"):
+    #         return snack
+    #     elif (snack == "gouda"):
+    #         return snack
+    #     elif (snack == "camembert"):
+    #         return snack
+    # else:
+    #     None
+    lst = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in lst: 
+            return snack
+    else:
+        return None
